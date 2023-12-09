10b0ee07811e759943a7381213c51c8c1c8e8b61
dhruvpattel <dhruvraghubhai@gmail.com>
2023-12-09 11:15:37 -0500

    classes and operand created

--- src/ArithmeticOperation.java
+++ src/ArithmeticOperation.java
@@ -4,6 +4,85 @@ abstract class ArithmeticExp {
     public abstract int evaluate();
     public abstract String toString();
 }
+// left and right operands
+abstract class Binary extends ArithmeticExp {
+    protected ArithmeticExp left;
+    protected ArithmeticExp right;
+
+    public Binary(ArithmeticExp left, ArithmeticExp right) {
+        this.left = left;
+        this.right = right;
+    }
+}
+
+// Number class
+class Number extends ArithmeticExp {
+    private int value;
+
+    public Number(int value) {
+        this.value=value;
+    }
+    @Override
+    public int evaluate() {
+        return value;
+    }
+    @Override
+    public String toString() {
+        return String.valueOf(value);
+    }
+}
+
+// Sum class
+class Sum extends Binary {
+    public Sum(ArithmeticExp left,ArithmeticExp right) {
+        super(left,right);
+    }
+
+    @Override
+    public int evaluate() {
+        return left.evaluate() + right.evaluate();
+    }
+
+    @Override
+    public String toString() {
+        return "(" + left.toString() + " + " + right.toString() + ")";
+    }
+}
+
+// Product class
+class Product extends Binary {
+    public Product(ArithmeticExp left,ArithmeticExp right) {
+        super(left, right);
+    }
+
+    @Override
+    public int evaluate() {
+        return left.evaluate()*right.evaluate();
+    }
+
+    @Override
+    public String toString() {
+        return "(" + left.toString() + " * " + right.toString() + ")";
+    }
+}
+
+// Modulo class
+class Modulo extends Binary {
+    public Modulo(ArithmeticExp left,ArithmeticExp right) {
+        super(left, right);
+    }
+
+    @Override
+    public int evaluate() {
+        return left.evaluate() %right.evaluate();
+    }
+
+    @Override
+    public String toString() {
+        return "(" + left.toString() + " % " + right.toString() + ")";
+    }
+}
+
 class ArithmeticOperation {
     /*Constants for representing the types*/
     public static final int TYPE_NUMBER = 1;
